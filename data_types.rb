692db24e4639feaa4b5aeb7b13327f44e34a63d9
Lachlan Fletcher <lach.fletcher@gmail.com>
2016-09-27 19:26:25 -0700

    Added data types.

---
+++ data_types.rb
@@ -0,0 +1,10 @@
+zest = true
+radfood: Za
+large = 18
+pizza_review = "It is empirically the best food."
+toppings = ["Anchovies", "Pineapple", "Potato"]
+order = {
+  size: large
+  flavor: "Good"
+  grade: 100
+}
