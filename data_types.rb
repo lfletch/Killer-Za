a405b108836a0d2d5dae417cadc986e25e85475d
Lachlan Fletcher <lach.fletcher@gmail.com>
2016-09-27 19:39:03 -0700

    Added data types correctly

--- data_types.rb
+++ data_types.rb
@@ -1,10 +1,15 @@
 zest = true
-radfood: Za
 large = 18
 pizza_review = "It is empirically the best food."
 toppings = ["Anchovies", "Pineapple", "Potato"]
 order = {
-  size: large
-  flavor: "Good"
-  grade: 100
+  size: large,
+  flavor:"Good",
+  grade:100
 }
+
+puts zest
+puts large
+puts toppings
+puts pizza_review
+puts order
